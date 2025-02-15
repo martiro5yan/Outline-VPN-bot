6cce611d2a037b54bbe9e8f94e26ee6670728208
martiri5yan <martiro5yan@vk.com>
2025-02-15 13:51:25 +0300

    hotfix

--- start_at_timer.py
+++ start_at_timer.py
@@ -3,6 +3,7 @@ import requests
 
 
 def start_timer(user_id):
+    print('Старт таймера')
     os.system(f"echo 'python delete_key_and_subscription.py {user_id}' | at now +1 minute")
 
 # def create_notification(user_id):
