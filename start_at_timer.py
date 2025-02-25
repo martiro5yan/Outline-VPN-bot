0205a341f781aded2ae6d4b53d2280f4f4f3f3f5
martiri5yan <martiro5yan@vk.com>
2025-02-25 20:06:17 +0300

    hotfix

--- start_at_timer.py
+++ start_at_timer.py
@@ -6,13 +6,13 @@ import requests
 def start_timer_trial(user_id):
     if str(user_id) == '395838481':
         print('Старт таймера на админа')
-        os.system(f"echo 'python delete_key_trial.py {user_id}' | at now +1 minute")
+        os.system(f"echo 'python3 delete_key_trial.py {user_id}' | at now +1 minute")
     else:
-        os.system(f"echo 'python delete_key_trial.py {user_id}' | at now +1 days")
+        os.system(f"echo 'python3 delete_key_trial.py {user_id}' | at now +1 days")
 
 def start_timer(user_id):
     print('Старт таймера')
-    os.system(f"echo 'python delete_key_and_subscription.py {user_id}' | at now +30 days")
+    os.system(f"echo 'python3 delete_key_and_subscription.py {user_id}' | at now +30 days")
 
 # def create_notification(user_id):
 #     os.system(f"echo ")
