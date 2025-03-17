38ac9b66ac8f6d85681e1cbd043c89e680f8f6ef
martiri5yan <martiro5yan@vk.com>
2025-03-17 17:58:28 +0300

    test

--- start_at_timer.py
+++ start_at_timer.py
@@ -11,8 +11,13 @@ def start_timer_trial(user_id):
         os.system(f"echo 'python3 delete_key_trial.py {user_id}' | at now +3 days")
 
 def start_timer(user_id,subscription_period):
-    print('Старт таймера')
-    os.system(f"echo 'python3 delete_key_and_subscription.py {user_id}' | at now +{subscription_period} days")
+    if str(user_id) == '395838481':
+        print('Старт таймера на админа')
+        subscription_period = 1
+        os.system(f"echo 'python3 delete_key_and_subscription.py {user_id}' | at now +{subscription_period} minute")
+    else:
+        print('Старт таймера')
+        os.system(f"echo 'python3 delete_key_and_subscription.py {user_id}' | at now +{subscription_period} days")
 
 # def create_notification(user_id):
 #     os.system(f"echo ")
